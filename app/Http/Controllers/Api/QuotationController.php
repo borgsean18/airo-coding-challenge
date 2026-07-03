08fcb1b5b60ea56acd4c0882e74e543dc0db85d7
Sean <seanborg8@gmail.com>
2026-07-03 16:03:08 +0100

    fixes

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\QuotationCalculator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class QuotationController extends Controller
 {
@@ -16,20 +16,23 @@ class QuotationController extends Controller
 
     public function provide_quotation(Request $request, QuotationCalculator $calculator)
     {
+        $allowed_currencies = ["EUR", "USD", "GBP"];
+
         $validated = $request->validate([
             'ages' => ['required', 'string'],
-            'currency_id' => ['required', 'string', 'size:3', 'regex:/^[A-Z]{3}$/'],
+            'currency_id' => ['required', 'string', Rule::in($allowed_currencies)],
             'start_date' => ['required', 'date_format:Y-m-d'],
             'end_date' => ['required', 'date_format:Y-m-d', 'after:start_date'],
         ]);
 
         $total = $calculator->calculate_quotation($validated);
+        $uniqueQuotationID = uniqid(); // This ID would be saved to a database
 
         // JSON Response - Total, currency_id, quotation_id
         return response()->json([
             'total' => $total,
             'currency_id' => $validated["currency_id"],
-            'quotation_id' => 1
+            'quotation_id' => $uniqueQuotationID
         ]);
     }
 }
